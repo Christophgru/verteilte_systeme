ccda4cbb473e14c513c95c4792ca669e690484b0
Yannic Grafwallner <grafyan@googlemail.com>
2025-10-18 17:34:58 +0200

    removed unused imports

--- uebung1.2/CalculationServer.java
+++ uebung1.2/CalculationServer.java
@@ -1,7 +1,3 @@
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
-import java.net.SocketTimeoutException;
 import java.util.ArrayList;
 import java.util.List;
 
